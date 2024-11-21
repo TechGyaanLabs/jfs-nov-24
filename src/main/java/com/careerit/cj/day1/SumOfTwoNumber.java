56b3d1df676e80be024933aeef2acdbcd051fff9
TechGyaanLabs <techgyaanlabs@gmail.com>
2024-11-21 06:50:18 +0530

    Update SumOfTwoNumber.java
    Added sum and avg

--- src/main/java/com/careerit/cj/day1/SumOfTwoNumber.java
+++ src/main/java/com/careerit/cj/day1/SumOfTwoNumber.java
@@ -6,7 +6,9 @@ public class SumOfTwoNumber {
         int num1 = 10;
         int num2 = 20;
         int sum = num1 + num2;
-        System.out.println("The sum of " + num1 + " and " + num2 + " is " + sum);
-
+        float avg   = sum / 2.0f;
+        System.out.println("Num1 = "+num1+" and Num2 = "+num2);
+        System.out.println("Sum is "+sum);
+        System.out.prinln("Avg is "+avg);
     }
 }
