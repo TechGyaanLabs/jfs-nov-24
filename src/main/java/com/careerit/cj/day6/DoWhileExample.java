0f580e4d778a4401b5859d399dbfef224269b497
TechGyaanLabs <techgyaanlabs@gmail.com>
2024-11-30 06:54:48 +0530

    added do while example

---
+++ src/main/java/com/careerit/cj/day6/DoWhileExample.java
@@ -0,0 +1,12 @@
+package com.careerit.cj.day6;
+
+public class DoWhileExample {
+    public static void main(String[] args) {
+
+            int i = 1;
+            do{
+                System.out.println(i);
+                i++;
+            }while(i <= 10);
+    }
+}
