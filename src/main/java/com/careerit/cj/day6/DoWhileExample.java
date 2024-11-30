eeedadb95c8befd287dec249dedefd681150250a
TechGyaanLabs <techgyaanlabs@gmail.com>
2024-11-30 07:07:52 +0530

    added do while example

--- src/main/java/com/careerit/cj/day6/DoWhileExample.java
+++ src/main/java/com/careerit/cj/day6/DoWhileExample.java
@@ -2,11 +2,17 @@ package com.careerit.cj.day6;
 
 public class DoWhileExample {
     public static void main(String[] args) {
+           int fact = factorial(5);
+           System.out.println("Factorial of 5 is :"+fact);
+    }
 
-            int i = 1;
-            do{
-                System.out.println(i);
-                i++;
-            }while(i <= 10);
+    public static int factorial(int num) {
+        int res = 1;
+        int i = num;
+        do {
+            res *= i;
+            i--;
+        } while (i >= 2);
+        return res;
     }
 }
