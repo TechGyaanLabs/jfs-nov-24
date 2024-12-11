972db6acfb329e5cbebffffdb982c2f4cc498898
TechGyaanLabs <techgyaanlabs@gmail.com>
2024-12-11 07:08:53 +0530

    Added enum example

---
+++ src/main/java/com/careerit/cj/day10/one/C.java
@@ -0,0 +1,11 @@
+package com.careerit.cj.day10.one;
+
+public class C extends A {
+
+    public void show() {
+        System.out.println(pub_num);
+        System.out.println(pro_num);
+        System.out.println(def_num);
+        //System.out.println(pri_num);
+    }
+}
